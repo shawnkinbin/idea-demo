a704193b777ab65d194b2bbe9bcab6cf6e5346ca
shao <shao@163.com>
2025-08-24 15:55:59 +0800

    test1

--- src/main/java/com/shao/wiki/Demo.java
+++ src/main/java/com/shao/wiki/Demo.java
@@ -3,6 +3,6 @@ package com.shao.wiki;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
-//        System.out.println("hello");
+        System.out.println("hello");
     }
 }
